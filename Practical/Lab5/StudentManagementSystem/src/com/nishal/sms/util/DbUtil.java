4b291e72c65d6e512f4efa587e17968eb5a76366
Rabita Chhantyal <chhantyalrabita@gmail.com>
2022-01-19 18:54:44 +0545

    student management system added

---
+++ Practical/Lab5/StudentManagementSystem/src/com/nishal/sms/util/DbUtil.java
@@ -0,0 +1,20 @@
+package com.nishal.sms.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtil {
+	private static final String DRIVER_NAME="com.mysql.cj.jdbc.Driver";
+	private static final String DATABASE_NAME="studentmgmt";
+	private static final String URL="jdbc:mysql://localhost:8889/";
+	private static final String USERNAME="root";
+	private static final String PASSWORD="root";
+	
+	public static Connection getConnection() throws SQLException, ClassNotFoundException
+	{
+		Class.forName(DRIVER_NAME);
+		return DriverManager.getConnection(URL + DATABASE_NAME + "?useSSL=false", USERNAME, PASSWORD);
+	}
+
+}
